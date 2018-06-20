af5b962c809570f2c3b0b647f2a1f7391c5df85c
Basil <abu.mahdi2008@gmail.com>
2018-06-21 02:10:22 +0300

    CRUD for Question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\QuestionResource;
 use App\Model\Question;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class QuestionController extends Controller
 {
@@ -15,6 +17,7 @@ class QuestionController extends Controller
     public function index()
     {
         //
+        return QuestionResource::collection(Question::latest()->get());
     }
 
     /**
@@ -35,7 +38,16 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // they are good as well
+//        $question = new Question();
+//        $question->title = $request->title;
+//        $question->slug = $request->slug;
+//        $question->body = $request->body;
+//
+//        $question->save();
+
+        Question::create($request->all());
+        return response('Created', Response::HTTP_CREATED);
     }
 
     /**
@@ -47,6 +59,7 @@ class QuestionController extends Controller
     public function show(Question $question)
     {
         //
+        return new QuestionResource($question);
     }
 
     /**
@@ -81,5 +94,8 @@ class QuestionController extends Controller
     public function destroy(Question $question)
     {
         //
+        $question->delete();
+//        return response('Deleted', Response::HTTP_NO_CONTENT);    // this one also is correct
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }

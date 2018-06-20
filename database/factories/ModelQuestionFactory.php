03878654a4066d083af2f260ea3038c136a43197
Basil <abu.mahdi2008@gmail.com>
2018-06-21 00:08:39 +0300

    factory & seeding

--- database/factories/ModelQuestionFactory.php
+++ database/factories/ModelQuestionFactory.php
@@ -3,7 +3,18 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Question::class, function (Faker $faker) {
+    $title = $faker->sentences;
+    $slug = str_slug($title);
     return [
         //
+        'title' => $title,
+        'slug' => $slug,
+        'body' => $faker->text(),
+        'category_id' => function() {
+            return \App\Model\Category::all()->random();
+        },
+        'user_id' => function() {
+            return \App\User::all()->random();
+        },
     ];
 });
